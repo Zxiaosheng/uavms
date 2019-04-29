b936fa0cd0b3f9a8ec5ccbb50a76ed467ef4c572
zhangxs139 <zhangxs139@chinaunicom.cn>
2019-04-29 11:58:34 +0800

    add mock

---
+++ mock/task.js
@@ -0,0 +1,38 @@
+import Mock from 'mockjs'
+
+const List = [];
+const count = 1000;
+
+for(let i = 0; i < count; i++){
+  List.push(Mock.mock({
+    taskId:'@increment',
+    taskName:'@ctitle(4,8)',
+    taskDesc:'@csentence(5, 12)',
+    "taskTypes|1":[{typeId:1,typeName:'消防任务'},{typeId:2,typeName:'医疗任务'},{typeId:3,typeName:'物流任务'},{typeId:4,typeName:'物流任务'},{typeId:5,typeName:'巡警任务'},{typeId:6,typeName:'其他任务'}],
+    "runModes|1":[{modeId:1,modeName:'消防任务'},{modeId:2,modeName:'医疗任务'},{modeId:3,modeName:'物流任务'},{modeId:4,modeName:'物流任务'},{modeId:5,modeName:'巡警任务'}],
+    beforeTask:'@ctitle(4,8)',
+    afterTask:'@ctitle(4,8)',
+    parentTask:'@ctitle(4,8)',
+    startTime:'@date',
+    endTime:'@date',
+    'taskUavs|1-5':['闪电F-28','科农A6-160','闪电F-35','猎鹰M6-84M6-84','天鹰M4-100','长空CK1B','长空CK1C','无侦5','ASN-12','WZ-2000','鲨鱼II'],
+    'taskStatus|1': ['Finished', 'outTime', 'Cancel','Pause'],
+    head:'@cname'
+  }))
+}
+
+export default [
+  {
+    url: '/task/list',
+    type: 'get',
+    response: () => {
+      return {
+        code: 20000,
+        data: {
+          total: List.length,
+          items: List
+        }
+      }
+    }
+  }
+]
