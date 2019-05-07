17adade27d4f06cc47a517dd3d258ef5e6d59193
OREO <3054964458@qq.com>
2019-05-07 19:42:33 +0800

    修改图

--- src/views/rode/rode-echart.vue
+++ src/views/rode/rode-echart.vue
@@ -3,10 +3,10 @@
 
     <el-row :gutter="20">
       <el-col :span="12">
-        <div :id="id" class="grid-content" :class="className" style="height:430px;width:100%;padding: 0 20px 0 20px"/>
+        <RodeChartPie></RodeChartPie>
       </el-col>
       <el-col :span="12">
-        <div :id="id1" class="grid-content" :class="className1" style="height:430px;width:100%;padding: 0 20px 0 20px"/>
+        <RodeChartBar></RodeChartBar>
       </el-col>
     </el-row>
 
@@ -27,6 +27,8 @@
   import 'echarts/map/js/china.js'
   import resize from '../../components/Charts/mixins/resize'
   import { fetchChartList,fetchMChartList } from '@/api/rode-echart'
+  import RodeChartPie from '@/components/Charts/RodeChartPie'
+  import RodeChartBar from '@/components/Charts/RodeChartBar'
   export default {
     name: "rode-echart",
     mixins: [resize],
@@ -63,247 +65,248 @@
         list:[]
       }
     },
+    components: { RodeChartPie ,RodeChartBar},
     mounted() {
 
       var colors = ['#5793f3', '#d14a61', '#675bba'];
 
-      this.chart = echarts.init(document.getElementById(this.id));
-      this.chart1 = echarts.init(document.getElementById(this.id1));
+      // this.chart = echarts.init(document.getElementById(this.id));
+      // this.chart1 = echarts.init(document.getElementById(this.id1));
       this.chart2 = echarts.init(document.getElementById(this.id2));
 
 
       //饼图
-      fetchChartList().then(response => {
-        this.list = response.data.items
+      // fetchChartList().then(response => {
+      //   this.list = response.data.items
+      //
+      //   this.chart.setOption({
+      //     backgroundColor: 'rgb(31,45,41)',
+      //
+      //     title: {
+      //       text: '福州市五区飞行路线总数统计饼图',
+      //       left: 'center',
+      //       top: 20,
+      //       textStyle: {
+      //         color: '#ffffff'
+      //       }
+      //     },
+      //     // grid:{
+      //     //   width:'100%'
+      //     // },
+      //
+      //     tooltip: {
+      //       trigger: 'item',
+      //       formatter: "{a} <br/>{b} : {c} ({d}%)"
+      //     },
+      //
+      //     visualMap: {
+      //       show: false,
+      //       min: 80,
+      //       max: 600,
+      //       inRange: {
+      //         colorLightness: [0, 1]
+      //       }
+      //     },
+      //     series: [
+      //       {
+      //         name: '飞行航程',
+      //         type: 'pie',
+      //         radius: '55%',
+      //         center: ['50%', '50%'],
+      //         data:this.list
+      //         //   [
+      //         //   {value:300,name:'仓山区'},
+      //         //   {value:332,name:'晋安区'},
+      //         //   {value:325,name:'鼓楼区'},
+      //         //   {value:432,name:'台江区'},
+      //         //   {value:402,name:'马尾区'}
+      //         // ]
+      //           .sort(function (a, b) {
+      //             return a.value - b.value;
+      //           }),
+      //         roseType: 'radius',
+      //         label: {
+      //           normal: {
+      //             textStyle: {
+      //               color: '#ffffff'
+      //             }
+      //           }
+      //         },
+      //         labelLine: {
+      //           normal: {
+      //             lineStyle: {
+      //               color: '#ffffff'
+      //             },
+      //             smooth: 0.2,
+      //             length: 10,
+      //             length2: 20
+      //           }
+      //         },
+      //         itemStyle: {
+      //           normal: {
+      //             color: '#0f375f',
+      //             // shadowBlur: 200,
+      //             // shadowColor: 'rgba(0, 0, 0,0.5)'
+      //           }
+      //         },
+      //
+      //         animationType: 'scale',
+      //         animationEasing: 'elasticOut',
+      //         animationDelay: function (idx) {
+      //           return Math.random() * 200;
+      //         }
+      //       }
+      //     ]
+      //   })
+      // })
 
-        this.chart.setOption({
-          backgroundColor: 'rgb(31,45,41)',
-
-          title: {
-            text: '福州市五区飞行路线总数统计饼图',
-            left: 'center',
-            top: 20,
-            textStyle: {
-              color: '#ffffff'
-            }
-          },
-          // grid:{
-          //   width:'100%'
-          // },
-
-          tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-          },
-
-          visualMap: {
-            show: false,
-            min: 80,
-            max: 600,
-            inRange: {
-              colorLightness: [0, 1]
-            }
-          },
-          series: [
-            {
-              name: '飞行航程',
-              type: 'pie',
-              radius: '55%',
-              center: ['50%', '50%'],
-              data:this.list
-              //   [
-              //   {value:300,name:'仓山区'},
-              //   {value:332,name:'晋安区'},
-              //   {value:325,name:'鼓楼区'},
-              //   {value:432,name:'台江区'},
-              //   {value:402,name:'马尾区'}
-              // ]
-                .sort(function (a, b) {
-                  return a.value - b.value;
-                }),
-              roseType: 'radius',
-              label: {
-                normal: {
-                  textStyle: {
-                    color: '#ffffff'
-                  }
-                }
-              },
-              labelLine: {
-                normal: {
-                  lineStyle: {
-                    color: '#ffffff'
-                  },
-                  smooth: 0.2,
-                  length: 10,
-                  length2: 20
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: '#0f375f',
-                  // shadowBlur: 200,
-                  // shadowColor: 'rgba(0, 0, 0,0.5)'
-                }
-              },
-
-              animationType: 'scale',
-              animationEasing: 'elasticOut',
-              animationDelay: function (idx) {
-                return Math.random() * 200;
-              }
-            }
-          ]
-        })
-      })
-
-      //某某地区飞行数据实时展示
-      fetchMChartList().then(response =>{
-        let list1=response.data.items
-
-      let option1 = {
-        backgroundColor: 'rgb(31,45,41)',
-        textStyle: {
-          color: '#fff'
-        },
-        title: {
-          text: '飞行数据实时展示',
-          top:'10',
-          textStyle: {
-            color: '#fff'
-          }
-        },
-        // grid:{
-        //   width:'100%'
-        // },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: 'rgba(20,200,212,0.3)'
-            }
-          }
-        },
-        legend: {
-          data:['飞行路线数', '飞行机器数'],
-          top:'10',
-          textStyle: {
-            color: '#fff'
-          }
-        },
-        toolbox: {
-          show: true,
-          feature: {
-            dataView: {readOnly: false},
-            restore: {},
-            saveAsImage: {}
-          }
-        },
-        dataZoom: {
-          show: false,
-          start: 0,
-          end: 100
-        },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: true,
-            data: (function (){
-              var now = new Date();
-              var res = [];
-              var len = 10;
-              while (len--) {
-                res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
-                now = new Date(now - 2000);
-              }
-              return res;
-            })()
-          },
-          {
-            type: 'category',
-            boundaryGap: true,
-            data: (function (){
-              var res = [];
-              var len = 10;
-              while (len--) {
-                res.push(10 - len - 1);
-              }
-              return res;
-            })()
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value',
-            scale: true,
-            name: '路线总数',
-            max: 30,
-            min: 0,
-            boundaryGap: [0.2, 0.2],
-
-          },
-          {
-            type: 'value',
-            scale: true,
-            name: '机器总数',
-            max: 1200,
-            min: 0,
-            boundaryGap: [0.2, 0.2]
-          }
-        ],
-        series: [
-          {
-            name:'飞行路线数',
-            type:'bar',
-            xAxisIndex: 1,
-            yAxisIndex: 1,
-            color:'rgb(44,133,222)',
-            data:(function (){
-              var res = [];
-              var len = 10;
-              while (len--) {
-                res.push(list1.m);
-
-              }
-              return res;
-            })()
-          },
-          {
-            name:'飞行机器数',
-            type:'line',
-            data:(function (){
-              var res = [];
-              var len = 0;
-              while (len < 10) {
-                res.push(list1.r);
-                len++;
-              }
-              return res;
-            })()
-          }
-        ]
-      };
-      this.chart1.setOption(option1);
-      app.count = 11;
-      setInterval(()=>{
-        let axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
-
-        var data0 = option1.series[0].data;
-        var data1 = option1.series[1].data;
-        data0.shift();
-        data0.push(Math.round(Math.random() * 1000));
-        data1.shift();
-        data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
-
-        option1.xAxis[0].data.shift();
-        option1.xAxis[0].data.push(axisData);
-        option1.xAxis[1].data.shift();
-        option1.xAxis[1].data.push(app.count++);
-
-        this.chart1.setOption(option1);
-      }, 2100);
-      })
+      // //某某地区飞行数据实时展示
+      // fetchMChartList().then(response =>{
+      //   let list1=response.data.items
+      //
+      // let option1 = {
+      //   backgroundColor: 'rgb(31,45,41)',
+      //   textStyle: {
+      //     color: '#fff'
+      //   },
+      //   title: {
+      //     text: '飞行数据实时展示',
+      //     top:'10',
+      //     textStyle: {
+      //       color: '#fff'
+      //     }
+      //   },
+      //   // grid:{
+      //   //   width:'100%'
+      //   // },
+      //   tooltip: {
+      //     trigger: 'axis',
+      //     axisPointer: {
+      //       type: 'cross',
+      //       label: {
+      //         backgroundColor: 'rgba(20,200,212,0.3)'
+      //       }
+      //     }
+      //   },
+      //   legend: {
+      //     data:['飞行路线数', '飞行机器数'],
+      //     top:'10',
+      //     textStyle: {
+      //       color: '#fff'
+      //     }
+      //   },
+      //   toolbox: {
+      //     show: true,
+      //     feature: {
+      //       dataView: {readOnly: false},
+      //       restore: {},
+      //       saveAsImage: {}
+      //     }
+      //   },
+      //   dataZoom: {
+      //     show: false,
+      //     start: 0,
+      //     end: 100
+      //   },
+      //   xAxis: [
+      //     {
+      //       type: 'category',
+      //       boundaryGap: true,
+      //       data: (function (){
+      //         var now = new Date();
+      //         var res = [];
+      //         var len = 10;
+      //         while (len--) {
+      //           res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+      //           now = new Date(now - 2000);
+      //         }
+      //         return res;
+      //       })()
+      //     },
+      //     {
+      //       type: 'category',
+      //       boundaryGap: true,
+      //       data: (function (){
+      //         var res = [];
+      //         var len = 10;
+      //         while (len--) {
+      //           res.push(10 - len - 1);
+      //         }
+      //         return res;
+      //       })()
+      //     }
+      //   ],
+      //   yAxis: [
+      //     {
+      //       type: 'value',
+      //       scale: true,
+      //       name: '路线总数',
+      //       max: 30,
+      //       min: 0,
+      //       boundaryGap: [0.2, 0.2],
+      //
+      //     },
+      //     {
+      //       type: 'value',
+      //       scale: true,
+      //       name: '机器总数',
+      //       max: 1200,
+      //       min: 0,
+      //       boundaryGap: [0.2, 0.2]
+      //     }
+      //   ],
+      //   series: [
+      //     {
+      //       name:'飞行路线数',
+      //       type:'bar',
+      //       xAxisIndex: 1,
+      //       yAxisIndex: 1,
+      //       color:'rgb(44,133,222)',
+      //       data:(function (){
+      //         var res = [];
+      //         var len = 10;
+      //         while (len--) {
+      //           res.push(list1.m);
+      //
+      //         }
+      //         return res;
+      //       })()
+      //     },
+      //     {
+      //       name:'飞行机器数',
+      //       type:'line',
+      //       data:(function (){
+      //         var res = [];
+      //         var len = 0;
+      //         while (len < 10) {
+      //           res.push(list1.r);
+      //           len++;
+      //         }
+      //         return res;
+      //       })()
+      //     }
+      //   ]
+      // };
+      // this.chart1.setOption(option1);
+      // app.count = 11;
+      // setInterval(()=>{
+      //   let axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
+      //
+      //   var data0 = option1.series[0].data;
+      //   var data1 = option1.series[1].data;
+      //   data0.shift();
+      //   data0.push(Math.round(Math.random() * 1000));
+      //   data1.shift();
+      //   data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
+      //
+      //   option1.xAxis[0].data.shift();
+      //   option1.xAxis[0].data.push(axisData);
+      //   option1.xAxis[1].data.shift();
+      //   option1.xAxis[1].data.push(app.count++);
+      //
+      //   this.chart1.setOption(option1);
+      // }, 2100);
+      // })
 
 
       // 预计飞行总数与实际飞行总数差距图
