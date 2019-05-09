2d2f8be47142188405fca0edfeb3cb5251632653
jimor <896792902@qq.com>
2019-05-10 02:00:07 +0800

    312'

--- src/views/dashboard/admin/index2.vue
+++ src/views/dashboard/admin/index2.vue
@@ -2,30 +2,30 @@
   <div class="dashboard-editor-container">
 
     <!--<panel-group @handleSetLineChartData="handleSetLineChartData" />-->
-    <el-row>
-      <p>
+    <el-row style="height: 8%;min-height: 1.5em" type="flex" align="middle" justify="center">
+      <div style="margin: auto;color: white">
         asdfgh
-      </p>
+      </div>
     </el-row>
 
     <el-row :gutter="40" style="height: 90%" type="flex" >
       <el-col :span="6" class="height100">
-        <el-row class="height50 i_box" style="margin-bottom: 10px">
-          <rode-chart id2="rode" height="100%" :cp="Number(1)"></rode-chart>
-        </el-row>
-        <el-row class="height50 i_box">
-          <gauge-chart id="gau" height="100%" width="100%"></gauge-chart>
-        </el-row>
 
+        <el-row class="height40 i_box">
+          <lineC id="lineC" height="100%" width="100%"></lineC>
+        </el-row>
+        <el-row class="height40 i_box" style="margin-bottom: 10px">
+          <bar-c id="barc" height="100%" width="100%"></bar-c>
+        </el-row>
       </el-col>
       <el-col :span="10" class="height100">
         <flypath id="flypath"></flypath>
       </el-col>
       <el-col :span="6" class="height100">
-        <el-row class="height30 i_box" style="margin-bottom: 10px">
+        <el-row class="height30 i_box" style="margin: 3% 0">
         <chart-c id="chartc" height="100%" width="100%"></chart-c>
         </el-row>
-        <el-row class="height30 i_box" style="margin-bottom: 10px">
+        <el-row class="height30 i_box" style="margin: 3% 0">
        <trouble id2="trouble" height="100%" width="100%"></trouble>
         </el-row>
         <el-row class="height30 i_box">
@@ -49,6 +49,8 @@
   import BarChart from '@/views/task/components/BarChart'
   import chartC from '@/components/Charts/IndexChartCricl'
   import trouble from '@/components/Charts/FirstTrouble'
+  import barC from '@/components/Charts/bar'
+  import lineC from '@/components/Charts/lineDraw'
 
   const lineChartData = {
     newVisitis: {
@@ -83,7 +85,9 @@
       hisCount,
       BarChart,
       chartC,
-      trouble
+      trouble,
+      barC,
+      lineC
     },
     data() {
       return {
@@ -136,8 +140,9 @@
     height: 100%;
   }
 
-  .height50 {
-       height: 50%;
+  .height40 {
+       height: 40%;
+    margin: 10% 0;
      }
   .height30 {
        height: 30%;
