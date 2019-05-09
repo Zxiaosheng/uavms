b74818881f8244ab82b3319848043408532d0ac5
chenss94 <chenss94@chinaunicom.cn>
2019-05-09 11:09:29 +0800

    111

--- src/components/Charts/HistorycountBar.vue
+++ src/components/Charts/HistorycountBar.vue
@@ -139,7 +139,7 @@
         }
       };
       let option = {
-        baseOption: {
+        baseOption:{
           backgroundColor: '#1F2D29',
           color: [
             '#7289AB','#73B9BC','#73A373','#fec42c', '#E69D87', '#C23531'
@@ -156,95 +156,7 @@
               }
             }
           },
-          timeline: {
-            itemStyle:{
-              normal:{
-                color:'#fff',
-                borderColor:'fff'
-              },
-              emphasis:{
-                color:'#fff',
-                borderColor:'fff'
-              }
-            },
-            lineStyle:{
-              color:'#fff',
-            },
-//              orient: 'vertical',
-//              left:'90%',
-//              x2:'0.3%',
-//              top:90,
-//              bottom:90,
-            axisType: 'category',
-            autoPlay: true,
-            playInterval: 2000,
-            data: [
-              '2016','2017',
-              {
-                value: '2018',
-                tooltip: {
-                  formatter: '{b} 飞行次数第一个里程碑'
-                },
-                symbol: 'diamond',
-                symbolSize: 16
-              }
-            ],
-            label: {
-              formatter : function(s) {
-                return (new Date(s)).getFullYear();
-              },
-              normal:{
-//                  position:'right',
-                color:'#fff',
-                fontSize:15,
-//                  rotate:'30',
-//                  lineHeight:30,
-              }
-            }
-          },
-          title: {
-            left: 'left',
-            top: 15,
-            textStyle: {
-              color: '#fff',
-              fontWeight: 'normal',
-              fontSize: 20
-            },
-            subtext: '数据来自中国联通统计小组',
-            left: 'left',
-            feature: {
-              mark: {show: true},
-              dataView: {show: true, readOnly: false},
-              magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
-              restore: {show: true},
-              saveAsImage: {show: true}
-            }
-          },
           tooltip:{},
-          legend: {
-            x: 'right',
-            top: 15,
-            textStyle:{
-              color:'#fff'
-            },
-            data: ['消防型','物流型','医疗型','天眼型','交通型','其它型'],
-//            selected: {
-//              '天眼型': false, '交通型': false, '其它型': false
-//            }
-          },
-          toolbox: {
-            show: true,
-            orient: 'vertical',
-            x:20,
-            top: 'center',
-            feature: {
-              mark: {show: true},
-              dataView: {show: true, readOnly: false},
-              magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
-              restore: {show: true},
-              saveAsImage: {show: true}
-            }
-          },
           calculable : true,
           grid: {
 //              x: '10%',
@@ -332,179 +244,307 @@
                 }
               }
             },
-            ]
+          ]
         },
-        options: [
-          {
-            title : {
-              text: '2016年无人机执行次数统计',
-            },
-            series : [
-              {data: dataMap.data1['2016'],
-                markPoint : {
-                data : [
-                  {type : 'max', name: '最大值'},
-                  {type : 'min', name: '最小值'}
-                ]
-              },},
-              {data: dataMap.data2['2016'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data3['2016'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data4['2016'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data5['2016'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data6['2016'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                }},
-              {data: [
-                {name: '消防型', value: dataMap.data1['2016sum']},
-                {name: '物流型', value: dataMap.data2['2016sum']},
-                {name: '医疗型', value: dataMap.data3['2016sum']},
-                {name: '交通型', value: dataMap.data4['2016sum']},
-                {name: '天眼型', value: dataMap.data5['2016sum']},
-                {name: '其它型', value: dataMap.data6['2016sum']}
-              ]}
-            ]
-          },
-          {
-            title : {text: '2017年无人机执行次数统计'},
-            series : [
-              {data: dataMap.data1['2017'],
-                markPoint : {
-                data : [
-                  {type : 'max', name: '最大值'},
-                  {type : 'min', name: '最小值'}
-                ]
-              },},
-              {data: dataMap.data2['2017'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data3['2017'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data4['2017'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data5['2017'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data6['2017'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: [
-                {name: '消防型', value: dataMap.data1['2017sum']},
-                {name: '物流型', value: dataMap.data2['2017sum']},
-                {name: '医疗型', value: dataMap.data3['2017sum']},
-                {name: '交通型', value: dataMap.data4['2016sum']},
-                {name: '天眼型', value: dataMap.data5['2016sum']},
-                {name: '其它型', value: dataMap.data6['2017sum']}
-              ]}
-            ]
-          },
-          {
-            title : {text: '2018年无人机执行次数统计'},
-            series : [
-              {data: dataMap.data1['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data2['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data3['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data4['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data5['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: dataMap.data6['2018'],
-                markPoint : {
-                  data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                  ]
-                },},
-              {data: [
-                {name: '消防型', value: dataMap.data1['2018sum']},
-                {name: '物流型', value: dataMap.data2['2018sum']},
-                {name: '医疗型', value: dataMap.data3['2018sum']},
-                {name: '交通型', value: dataMap.data4['2018sum']},
-                {name: '天眼型', value: dataMap.data5['2018sum']},
-                {name: '其它型', value: dataMap.data6['2018sum']}
-              ]}
-            ]
-          },
-
-        ]
+        options: [ ]
       };
+      if(window.location.href.includes('dashboard'))
+        {
+          option.options.push( {
+          series : [
+            {data: dataMap.data1['2016']},
+            {data: dataMap.data2['2016']},
+            {data: dataMap.data3['2016']},
+            {data: dataMap.data4['2016']},
+            {data: dataMap.data5['2016']},
+            {data: dataMap.data6['2016']},
+          ]
+        }, {
+          series : [
+            {data: dataMap.data1['2017']},
+            {data: dataMap.data2['2017']},
+            {data: dataMap.data3['2017']},
+            {data: dataMap.data4['2017']},
+            {data: dataMap.data5['2017']},
+            {data: dataMap.data6['2017']},
+          ]
+          }, {
+            series : [
+              {data: dataMap.data1['2018']},
+              {data: dataMap.data2['2018']},
+              {data: dataMap.data3['2018']},
+              {data: dataMap.data4['2018']},
+              {data: dataMap.data5['2018']},
+              {data: dataMap.data6['2018']},
+            ]
+          },);
+          option.baseOption.timeline={
+            autoPlay: true,
+            playInterval: 1000,
+            data: ['2016','2017','2018' ],
+
+          };
+          option.baseOption.title={};
+          option.baseOption.toolbox={};
+          option.baseOption.legend={
+            x: 'right',
+            top:15,
+            textStyle:{
+              color:'#fff'
+            },
+            data: ['消防型','物流型','医疗型','天眼型','交通型','其它型'],
+            selected: {
+              '天眼型': false, '交通型': false, '其它型': false
+            }
+          };
+        }else
+        {
+        option.options.push( {
+          title : {
+            text: '2016年无人机执行次数统计',
+          },
+          series : [
+            {data: dataMap.data1['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data2['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data3['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data4['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data5['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data6['2016'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              }},
+            {data: [
+              {name: '消防型', value: dataMap.data1['2016sum']},
+              {name: '物流型', value: dataMap.data2['2016sum']},
+              {name: '医疗型', value: dataMap.data3['2016sum']},
+              {name: '交通型', value: dataMap.data4['2016sum']},
+              {name: '天眼型', value: dataMap.data5['2016sum']},
+              {name: '其它型', value: dataMap.data6['2016sum']}
+            ]}
+          ]
+        }, {
+          title : {text: '2017年无人机执行次数统计'},
+          series : [
+            {data: dataMap.data1['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data2['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data3['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data4['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data5['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data6['2017'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: [
+              {name: '消防型', value: dataMap.data1['2017sum']},
+              {name: '物流型', value: dataMap.data2['2017sum']},
+              {name: '医疗型', value: dataMap.data3['2017sum']},
+              {name: '交通型', value: dataMap.data4['2016sum']},
+              {name: '天眼型', value: dataMap.data5['2016sum']},
+              {name: '其它型', value: dataMap.data6['2017sum']}
+            ]}
+          ]
+        }, {
+          title : {text: '2018年无人机执行次数统计'},
+          series : [
+            {data: dataMap.data1['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data2['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data3['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data4['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data5['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: dataMap.data6['2018'],
+              markPoint : {
+                data : [
+                  {type : 'max', name: '最大值'},
+                  {type : 'min', name: '最小值'}
+                ]
+              },},
+            {data: [
+              {name: '消防型', value: dataMap.data1['2018sum']},
+              {name: '物流型', value: dataMap.data2['2018sum']},
+              {name: '医疗型', value: dataMap.data3['2018sum']},
+              {name: '交通型', value: dataMap.data4['2018sum']},
+              {name: '天眼型', value: dataMap.data5['2018sum']},
+              {name: '其它型', value: dataMap.data6['2018sum']}
+            ]}
+          ]
+        },);
+          option.baseOption.timeline={
+            itemStyle:{
+              normal:{
+                color:'#fff',
+                borderColor:'fff'
+              },
+              emphasis:{
+                color:'#fff',
+                borderColor:'fff'
+              }
+            },
+            lineStyle:{
+              color:'#fff',
+            },
+            axisType: 'category',
+            autoPlay: true,
+            playInterval: 2000,
+            data: [
+              '2016','2017',
+              {
+                value: '2018',
+                tooltip: {
+                  formatter: '{b} 飞行次数第一个里程碑'
+                },
+                symbol: 'diamond',
+                symbolSize: 16
+              }
+            ],
+            label: {
+              formatter : function(s) {
+                return (new Date(s)).getFullYear();
+              },
+              normal:{
+                color:'#fff',
+                fontSize:15,
+              }
+            }
+          };
+          option.baseOption.title={
+            left: 'left',
+              top: 15,
+              textStyle: {
+              color: '#fff',
+                fontWeight: 'normal',
+                fontSize: 20
+            },
+            subtext: '数据来自中国联通统计小组',
+              left: 'left',
+              feature: {
+              mark: {show: true},
+              dataView: {show: true, readOnly: false},
+              magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+              restore: {show: true},
+              saveAsImage: {show: true}
+            }
+          };
+          option.baseOption.toolbox={
+            show: true,
+              orient: 'vertical',
+              x:20,
+              top: 'center',
+              feature: {
+              mark: {show: true},
+              dataView: {show: true, readOnly: false},
+              magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+              restore: {show: true},
+              saveAsImage: {show: true}
+            }
+          };
+          option.baseOption.legend={
+            x: 'right',
+              top: 15,
+              textStyle:{
+              color:'#fff'
+            },
+            data: ['消防型','物流型','医疗型','天眼型','交通型','其它型'],
+//            selected: {
+//              '天眼型': false, '交通型': false, '其它型': false
+//            }
+          };
+      }
       this.chart = echarts.init(document.getElementById(this.id))
       this.chart.setOption(option)
     }
