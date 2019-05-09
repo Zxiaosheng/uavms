f76ffa9803d3ad7d7d357d0bf08c50cd0f03bdb6
yinjw12 <yy1213822>
2019-05-09 14:55:47 +0800

    修改了飞行路线

--- src/router/modules/device.js
+++ src/router/modules/device.js
@@ -23,6 +23,12 @@ const deviceRouter = {
       component: () => import('@/views/device/dev-add'),
       name: 'DeviceAdd',
       meta: { title: 'addDevice' }
+    },
+    {
+      path: 'test',
+      component: () => import('@/components/Charts/RodeDynamic'),
+      name: 'test',
+      meta: { title: 'test' }
     }
   ]
 }
