22f5221fac4e7b2e3daf3c0c29e9416bcb8204dc
chenss94 <chenss94@chinaunicom.cn>
2019-05-10 10:12:55 +0800

    首页图表

--- src/components/Charts/FirstTrouble.vue
+++ src/components/Charts/FirstTrouble.vue
@@ -18,7 +18,8 @@
       },
       height: {
         type: String,
-        default: '430px'
+//        default: '430px'
+        default: '100%'
       },
       className2: {
         type: String,
@@ -59,117 +60,78 @@
         var lineData = [];
         var barData1 = [];
         var barData2 = [];
-        for (var i = 0; i < 30; i++) {
+        for (var i = 0; i < 10; i++) {
           var date = new Date(dottedBase += 1000 * 3600 * 24);
           category.push([
             date.getFullYear(),
             date.getMonth() + 1,
             date.getDate()
           ].join('-'));
-          // if (i<=10&&i>=1){
-          //     category.push(i);
-          // }
+
           var b = Math.random() * 200;
           var d = Math.random() * (-200);
           barData1.push(b)
           barData2.push(d)
         }
-
-
         let option2 = {
           backgroundColor: '#0A1123',
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'shadow',
-              label: {
-                show: true,
-                backgroundColor: '#333'
-              }
-            }
-          },
           legend: {
             data: ['已处理故障','未处理故障'],
+            left:'left',
             textStyle: {
-              color: '#ccc'
+              color: '#fff'
             }
           },
           xAxis: {
             type: 'category',
             data: category,
-            axisTick: {
-              alignWithLabel: true
+            textStyle:{
+              color:'#fff'
             },
-
             splitLine: {
               show: false,
 
             },
             axisLabel:{
-              //fontWeight:10,
-              //interval:2,
               fontsize:2,
-              align:'center',
-              color:'rgba(255,255,255,0.3)'
+              color:'rgb(255,255,255)'
             }
           },
           yAxis: [{
             type: 'value',
             splitLine: {
-              show: true,
               lineStyle:{
-                color:'rgba(255,255,255,0.2)'
+                color:'#fff'
               }
             },
-            axisLine:{
-              show:false
-            },
             axisLabel:{
-
               fontWeight:10,
               fontsize:5,
-              color:'rgba(255,255,255,0.3)'
+              color:'rgb(255,255,255)'
             }
-
           }],
           series: [{
             name: '已处理故障',
             type: 'bar',
             stack: '总量',
             barWidth: 10,
-            itemStyle: {
-              normal: {
-                barBorderRadius: 50,
-                color: new echarts.graphic.LinearGradient(
-                  0, 0, 0, 1,
-                  [
-                    {offset: 0, color: '#BC34BC'},
-                    {offset: 1, color: '#7F3594'}
-                  ]
-                )
-              }
+            itemStyle:{
+              color:'rgb(24,177,175)',
             },
             data: barData1
-          },{
-            name: '未处理故障',
-            type: 'bar',
-            stack: '总量',
-            barWidth: 10,
-            itemStyle: {
-              normal: {
-                barBorderRadius: 50,
-                color: new echarts.graphic.LinearGradient(
-                  0, 0, 0, 1,
-                  [
-                    {offset: 0, color: '#4740C8'},
-                    {offset: 1, color: '#EF71FF'}
-                  ]
-                )
-              }
-            },
-            data: barData2
-          }]
+          },
+            {
+              name: '未处理故障',
+              type: 'bar',
+              barWidth: '25%',
+              stack: '总量',
+              itemStyle:{
+                color:'rgb(15,109,205)',
+              },
+              data: barData2
+            }]
         };
+
         this.chart2.setOption(option2);
         window.addEventListener("resize",()=>{
           console.log('111')
