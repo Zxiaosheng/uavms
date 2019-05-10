26b9f5017e63a22cf04ce87a760ec554fc8eef63
wangb88 <wangb88@chinaunicom.cn>
2019-05-10 10:48:55 +0800

    1

--- src/views/flight/map.vue
+++ src/views/flight/map.vue
@@ -154,18 +154,17 @@ const SHData = [
   [{ name: '上海' }, { name: '大连', value: 10 }]
 ]
 
-var GZData = [
-  [{ name: '广州' }, { name: '福州', value: 95 }],
-  [{ name: '广州' }, { name: '太原', value: 90 }],
-  [{ name: '广州' }, { name: '长春', value: 80 }],
-  [{ name: '广州' }, { name: '重庆', value: 70 }],
-  [{ name: '广州' }, { name: '西安', value: 60 }],
-  [{ name: '广州' }, { name: '成都', value: 50 }],
-  [{ name: '广州' }, { name: '常州', value: 40 }],
-  [{ name: '广州' }, { name: '北京', value: 30 }],
-  [{ name: '广州' }, { name: '北海', value: 20 }],
-  [{ name: '广州' }, { name: '海口', value: 10 }],
-  [{ name: '广州' }, { name: '泉州', value: 10 }]
+const FZData = [
+  [{ name: '福州' }, { name: '太原', value: 90 }],
+  [{ name: '福州' }, { name: '长春', value: 80 }],
+  [{ name: '福州' }, { name: '重庆', value: 70 }],
+  [{ name: '福州' }, { name: '西安', value: 60 }],
+  [{ name: '福州' }, { name: '成都', value: 50 }],
+  [{ name: '福州' }, { name: '常州', value: 40 }],
+  [{ name: '福州' }, { name: '北京', value: 30 }],
+  [{ name: '福州' }, { name: '北海', value: 20 }],
+  [{ name: '福州' }, { name: '海口', value: 10 }],
+  [{ name: '福州' }, { name: '福州', value: 10 }]
 ]
 
 var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z'
@@ -190,7 +189,7 @@ var convertData = function(data) {
 }
 var color = ['#a6c84c', '#ffa022', '#46bee9']
 var series = [];
-[['北京', BJData], ['上海', SHData], ['广州', GZData]].forEach(function(item, i) {
+[['北京', BJData], ['上海', SHData], ['福州', FZData]].forEach(function(item, i) {
   // console.log(item,i);
   series.push({
     name: item[0] + ' Top10',
@@ -319,7 +318,7 @@ export default {
         orient: 'vertical',
         top: 'bottom',
         left: 'right',
-        data: ['北京 Top10', '上海 Top10', '广州 Top10'],
+        data: ['北京 Top10', '上海 Top10', '福州 Top10'],
         textStyle: {
           color: '#fff'
         },
