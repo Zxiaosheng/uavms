db2bbc5d91ff6c281556323621d512f39bb706f1
yinjw12 <yy1213822>
2019-05-22 19:37:51 +0800

    添加图表的展示

--- src/views/device/dev-chart.vue
+++ src/views/device/dev-chart.vue
@@ -22,10 +22,60 @@
     },
     computed: {
       res(){
-        return this.dev.res
+
+        let resTemp={
+          windRes: {
+            name: '抗风系数',
+            value: this.dev.windRes
+          },
+          waterRes: {
+            name: '抗水系数',
+            value: this.dev.waterRes
+          },
+          hitRes: {
+            name: '放撞击系数',
+            value: this.dev.hitRes
+          },
+          flexibility: {
+            name: '灵活系数',
+            value: this.dev.flexibility
+          },
+          emcRes: {
+            name: '抗电磁干扰系数',
+            value: this.dev.emcRes
+          }
+        }
+        return resTemp
       },
       loss(){
-        return this.dev.loss
+
+        let lossTemp = {
+          wingLoss: {
+            name: '机翼损耗率',
+            value: this.dev.wingLoss
+          },
+          powerLoss: {
+            name: '电池损耗率',
+            value: this.dev.powerLoss
+          },
+          cameraLoss: {
+            name: '摄像头损耗率',
+            value: this.dev.cameraLoss
+          },
+          packLoss: {
+            name: '机舱损耗率',
+            value: this.dev.packLoss
+          },
+          engineLoss: {
+            name: '引擎损耗率',
+            value: this.dev.engineLoss
+          },
+          baseLoss: {
+            name: '底座损耗率',
+            value: this.dev.baseLoss
+          }
+        }
+        return lossTemp
       }
     }
   }
