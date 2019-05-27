f7ea312e8928982578af9c9e23eec9efb11c6d49
liangzl14 <liangzl14@chinaunicom.cn>
2019-05-27 10:32:16 +0800

    update

--- src/api/trouble.js
+++ src/api/trouble.js
@@ -1,22 +1,24 @@
 import request from '@/utils/request'
 
 export function fetchNewsList(query) {
+  let { page,limit }=query;
+  let params={ page,limit }
   return request({
-    url: '/trouble/list',
+    url: '/serve/trouble/list',
     method: 'get',
-    params: query
+    params: params
   })
 }
 export function updateNews(data) {
   return request({
-    url: '/trouble/update',
+    url: '/serve/trouble/update',
     method: 'post',
     data
   })
 }
 export function createNews(data) {
   return request({
-    url: '/trouble/create',
+    url: '/serve/trouble/create',
     method: 'post',
     data
   })
