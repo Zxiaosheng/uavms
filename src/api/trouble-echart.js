d174a9358f15caea9eeca2fafbd0cb2467911473
liangzl14 <liangzl14@chinaunicom.cn>
2019-05-28 17:03:37 +0800

    update

--- src/api/trouble-echart.js
+++ src/api/trouble-echart.js
@@ -1,17 +1,24 @@
 import request from '@/utils/request'
 
 export function fetchChartList() {
+  console.log("===============")
   return request({
-    url: '/trouble/chartlist',
-    method: 'get',
+    url: '/dev-api/trouble/chartlist',
+    method: 'get'
     // params: query
   })
 }
 export function fetchMChartList(query) {
   return request({
-    url: '/trouble/mchartlist',
+    url: '/dev-api/trouble/mchartlist',
     method: 'get',
     params: query
   })
 }
-
+export function fetchthirdChartList(query) {
+  return request({
+    url: '/dev-api/trouble/thirdchartlist',
+    method: 'get',
+    params: query
+  })
+}
